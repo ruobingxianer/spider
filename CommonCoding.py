3683f5b90dcf464c40f6804da8230a2cfe343d6c
wanghx <wanghx@lib.pku.edu.cn>
2020-11-26 15:26:16 +0800

    add easy python

--- CommonCoding.py
+++ CommonCoding.py
@@ -1,4 +1,6 @@
+# -*- coding: UTF-8 -*-
 import requests
+import sys
 
 
 def getHtmlText(url):
@@ -6,11 +8,16 @@ def getHtmlText(url):
         r = requests.get(url, timeout=30)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
+        fo = open("test.txt", "w")
+        fo.write(r.text)
+        fo.close()
         return r.text
     except:
         return "exception"
 
 
 if __name__ == "__main__":
+    reload(sys)
+    sys.setdefaultencoding('utf8')
     url = "https://ctext.org/zh"
     print getHtmlText(url)
